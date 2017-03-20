7f1818771b69838c4595b76002568764e86e34ba
Xing Li <ljlx1120@gmail.com>
2017-03-20 13:50:27 -0700

    add debug

--- app/app.php
+++ app/app.php
@@ -3,7 +3,12 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Clock.php";
 
+  // use Symfony\Component\Debug\Debug;
+  // Debug::enable();
+
   $app = new Silex\Application();
+
+  // $app['debug'] = true;
 
   $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
@@ -14,7 +19,7 @@
   });
 
   $app->post("/calculate", function () use ($app) {
-    
+
     $new_clock = new Clock;
     $hour = $_POST['hour'];
     $min = $_POST['min'];
